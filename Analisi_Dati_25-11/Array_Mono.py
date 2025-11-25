dd3a4d96ee6b93d13a118233125070e28287a61a
annamagaldi12 <anna.magaldi12@gmail.com>
2025-11-25 17:39:39 +0100

    Create Array_Mono.py

---
+++ Analisi_Dati_25-11/Array_Mono.py
@@ -0,0 +1,18 @@
+#import libreria numpy
+import numpy as np
+
+arr = np.array([4,7,1,8,5])
+print("Array:", arr)
+
+#STATISTTICHE DI BASE
+#MINIMO:
+val_min = np.min(arr)
+print("Valore minimo:", val_min)
+#MAX:
+val_max = np.max(arr)
+print("Valore massimo:", val_max)
+#MEDIA:
+media = np.mean(arr)
+print("Media:", media)
+#DEVIAZIONE STANDARD:
+
